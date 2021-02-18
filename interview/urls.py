49bd5d21bf9d027483a31ab70fbea5986bdd969e
InKyeongBae <inkkyung@gmail.com>
2021-02-19 05:32:10 +0900

    [UPDATE] interview detail page 완성 & [FIX] interview 모델

--- interview/urls.py
+++ interview/urls.py
@@ -3,6 +3,8 @@ from .views import *
 app_name = 'interview'
 
 urlpatterns = [
-    path('interviewee/create/', interviewee_create, name="devtool_create" ),
-    path('', interview_list, name="interview_list"),
+    path('non_major/', non_major_interview, name="non_major_interview"),
+    path('major/', major_interview, name="major_interview"),
+    path('double_major/', double_major_interview, name="double_major_interview"),
+    path('view/<int:pk>/',interview_view, name="interview_view")
 ]
--- interview/views.py
+++ interview/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render, redirect
-from .forms import *
 from django.urls import reverse
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
-def interview_list(request):
-    interviewees = Interviewee.objects.all()
-    data={
-        'interviewees' : interviewees
-    }
-    return render(request, 'interview/interview.html',data )
+def non_major_interview(request) :
+    
+    return render(request,'interview/non_major_interview.html',data)
 
-def interviewee_create(request):
-    if request.method == 'POST':
-        form = IntervieweeForm(request.POST, request.FILES) 
-        if form.is_valid():  
-            post = form.save()  
-            return redirect('interview:interview_list') #추후 mainpage로 수정
-    else:  
-        form = IntervieweeForm()
-        ctx = {'form': form}
-        return render(request, 'interview/intervieweeform.html', ctx)
+def major_interview(request) :
+    interviewees = Answer.objects.filter(major = '전공자')
+    data = {
+        'interviewees' : interviewees,
+    }
+    return render(request,'interview/major_interview.html',data)
+
+def double_major_interview(request) :
+    
+    return render(request,'interview/double_major_interview.html',data)
+
+
+def interview_view(request, pk):
+    interview = Answer.objects.filter(pk=pk)
+    data={
+        'interview' : interview,
+    }
+    return render(request, 'interview/interview_view.html',data )
+
