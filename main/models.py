39b613e99c6e6f17494a1c5205d9f3b8269e6513
InjiChoi <chldlswl1212@gmail.com>
2021-02-20 06:38:44 +0900

    [UPDATE]main에 필요한 model 추가

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class MainDescImg(models.Model):
+    first_img = models.ImageField(null=True, blank=True, upload_to="main", verbose_name="메인 사진 1")
+    second_img = models.ImageField(null=True, blank=True, upload_to="main", verbose_name="메인 사진 2")
+    third_img = models.ImageField(null=True, blank=True, upload_to="main", verbose_name="메인 사진 3")
+
+    def __str__(self):
+        return 'Main Page Desc Images'
+
+class MainInterview(models.Model):
+    interview = models.ForeignKey('interview.Answer', on_delete=models.CASCADE, related_name="main_interviews")
+
+    def __str__(self):
+        return 'Main Page Interviews - {}'.format(self.interview)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render,redirect
+from .models import *
 
 def main_page(request):
-    return render(request,'main/main.html',{})
+    desc_img = MainDescImg.objects.first()
+    print(desc_img.second_img)
+    interviews = MainInterview.objects.all()[:4]
+    ctx = {
+        'desc_img':desc_img,
+        'interviews':interviews,
+    }
+    return render(request,'main/main.html', ctx)
