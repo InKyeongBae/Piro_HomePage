ee1c7407565c911bc55f2bffcd2320cbf21d2ab1
InjiChoi <chldlswl1212@gmail.com>
2021-02-20 18:15:05 +0900

    [FIX]main page desc image default 처

--- main/models.py
+++ main/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 # Create your models here.
 class MainDescImg(models.Model):
-    first_img = models.ImageField(null=True, blank=True, upload_to="main", verbose_name="메인 사진 1")
-    second_img = models.ImageField(null=True, blank=True, upload_to="main", verbose_name="메인 사진 2")
-    third_img = models.ImageField(null=True, blank=True, upload_to="main", verbose_name="메인 사진 3")
+    first_img = models.ImageField(null=True, blank=True, upload_to="main", verbose_name="메인 사진 1",
+                default='static/images/desc_1.png')
+    second_img = models.ImageField(null=True, blank=True, upload_to="main", verbose_name="메인 사진 2",
+                default='static/images/desc_2.png')
+    third_img = models.ImageField(null=True, blank=True, upload_to="main", verbose_name="메인 사진 3",
+                default='static/images/desc_3.png')
 
     def __str__(self):
         return 'Main Page Desc Images'
+
 
 class MainInterview(models.Model):
     interview = models.ForeignKey('interview.Answer', on_delete=models.CASCADE, related_name="main_interviews")
--- main/views.py
+++ main/views.py
@@ -3,7 +3,9 @@ from .models import *
 
 def main_page(request):
     desc_img = MainDescImg.objects.first()
-    print(desc_img.second_img)
+    if desc_img == None:
+        MainDescImg.objects.create()
+
     interviews = MainInterview.objects.all().order_by('-id')[:4]
     print(interviews)
     ctx = {
